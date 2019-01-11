bb09fea30f809ecd96398ef9ba47116ea7c3286c
sz_urbanik <sz.urbanik@mat.net.pl>
2019-01-11 12:09:24 +0100

    Creating db added Auto incrementation added to primary key in two tables Insert statement added

--- SqlQuery.sql
+++ SqlQuery.sql
@@ -1,18 +1,33 @@
+CREATE DATABASE Sklep;
+
+GO 
+
+USE Sklep;
+
+GO 
+
+
 CREATE TABLE osoba_prawna (
-    nazwa nvarchar(50) primary key,
-    nip varchar(10) ,
-	adres nvarchar(50),
-	nazwisko nvarchar(50),
-	telefon varchar(50),
-	email varchar(50)
+	id integer  IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	nazwisko nvarchar(50) NOT NULL,
+    nazwa nvarchar(50) NOT NULL,
+    nip varchar(10) NOT NULL ,
+	adres nvarchar(50) NOT NULL,
+	telefon varchar(9) NOT NULL,
+	email varchar(50) NOT NULL
 );
+
+
+insert into osoba_prawna (nazwisko, nazwa, nip, adres, telefon, email)
+values ('Nowak','Nowak Industries','2940640431','Pilotów 5/32','632055443','nowak@inustries.pl');
  GO
 
 CREATE TABLE osoba_fizyczna (
-    imie nvarchar(50) primary key,
-    nazwisko varchar(10) ,
+	id integer  IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	nazwisko nvarchar(50),
+	imie nvarchar(50) ,
 	adres nvarchar(50),
-	telefon varchar(50)
+	telefon varchar(50),
 );
 Go
 
@@ -35,4 +50,8 @@ Create Table faktura(
 	constraint faktura_komsetyk unique (kosmetyk),
 	cena integer,
 	ilosc_sztuk integer,
+	osoba_prawna integer references osoba_prawna(id),
+	constraint osoba_prawna_nazwa unique (osoba_prawna),
+	osoba_fizyczna integer references osoba_fizyczna(id),
+	constraint osoba_fizyczna_id unique (osoba_fizyczna),
 )
